8a9be3ffda641099a5991935b171b0c5cfb2ac7f
Tobias Herzog <tobias.herzog@tag24.de>
2021-07-01 10:54:41 +0200

    changed mount list-html

--- public/app/components/note.js
+++ public/app/components/note.js
@@ -24,14 +24,14 @@ const template = `
         <div class="col-2"></div>
         <div class="col-8">
             <div class="input-group  mb-1">
-                <button class="fas fa-angle-down align-bottom input-group-text collapsible"></button>
-                <input type="text" class="form-control input-group-text" placeholder="Title">
+                <button class="fas fa-angle-down align-bottom input-group-text collapsible collapsibleButton"></button>
+                <input type="text" class="form-control input-group-text inputField" placeholder="Title">
                 <button type="button" class="fas fa-edit btn btn-dark border border-primary change-note"></button>
                 <button type="button"
                         class="far fa-trash-alt btn btn-danger border border-primary delete-note"></button>
             </div>
             <div class="row">
-                <div class="col"><textarea class="form-control mb-2" placeholder="type your note here" rows="10"
+                <div class="col"><textarea class="form-control mb-2 textarea" placeholder="type your note here" rows="10"
                                            style="display: none;"></textarea>
                     <div class="html-view p-2" style="display: none;">
                     </div>
@@ -48,62 +48,33 @@ const template = `
  */
 function mount(note, $element) {
 
-    /*const $note =   createElementFromHTML(template);
-
-    $element.appendChild($note);
-    */
-    let firstrowdiv = document.createElement('div')
-    firstrowdiv.className = 'row mb-2 mt-2 note'
-
-    let firstcol2div = document.createElement('div')
-    firstcol2div.className = 'col-2'
-
-    let col8div = document.createElement('div')
-    col8div.className = 'col-8'
-
-    let firstcol6div = document.createElement('div')
-    firstcol6div.className = 'col'
-
-    let inputgroupdiv = document.createElement('div')
-    inputgroupdiv.className = 'input-group  mb-1'
-
-    let textarea = document.createElement('textarea')
-    textarea.className = 'form-control mb-2'
-    textarea.placeholder = 'type your note here'
-    textarea.style.display = 'none'
-    textarea.value = note.text
-    textarea.rows = 10;
-
-    let htmlView = document.createElement('div');
-    htmlView.className = 'html-view p-2';
-    htmlView.style.display = 'none';
-    htmlView.innerHTML = marked(note.text);
+    const $note = createElementFromHTML(template);
 
 
-    let collapsebutton = document.createElement('button')
-    collapsebutton.className = 'fas fa-angle-down align-bottom input-group-text collapsible'
-    collapsebutton.addEventListener('click', function () {
-        this.classList.toggle('active');
-        if (textarea.style.display === "block") {
-            textarea.style.display = "none"
-        } else {
-            textarea.style.display = "block"
+    let collapsibleButton = $note.querySelector('.collapsibleButton');
+
+    let htmlView = $note.querySelector('.html-view');
+
+    let textarea = $note.querySelector('.textarea');
+
+    let input = $note.querySelector('.inputField');
+
+    collapsibleButton.addEventListener('click', function () {
+            this.classList.toggle('active');
+            if (textarea.style.display === "block") {
+                textarea.style.display = "none"
+            } else {
+                textarea.style.display = "block"
+            }
         }
-    })
-
-    collapsebutton.addEventListener('mouseover', _ => {
+    );
+    collapsibleButton.addEventListener('mouseover', _ => {
         htmlView.style.display = 'block';
     });
+    textarea.value = note.text;
+    htmlView.innerHTML = marked(note.text);
 
-    // let icon = document.createElement('i')
-
-
-    let input = document.createElement('input')
-    input.type = 'text'
-    input.className = 'form-control input-group-text'
-    input.placeholder = 'Title'
-    input.value = note.title
-
+    input.value = note.title;
     input.addEventListener('mouseover', _ => {
         htmlView.style.display = 'block';
     });
@@ -112,48 +83,16 @@ function mount(note, $element) {
         htmlView.style.display = 'none';
     });
 
-
-    let changebutton = document.createElement('button')
-    changebutton.type = 'button'
-    changebutton.className = 'fas fa-edit btn btn-dark border border-primary change-note'
-    changebutton.addEventListener('click', _ => {
+    $note.querySelector('.change-note').addEventListener('click', _ => {
         note.title = input.value;
         note.text = textarea.value;
     });
 
-
-    let deletebutton = document.createElement('button')
-    deletebutton.type = 'button'
-    deletebutton.className = 'far fa-trash-alt btn btn-danger border border-primary delete-note'
-    deletebutton.addEventListener('click', _ => {
+    $note.querySelector('.delete-note').addEventListener('click', _ => {
         note.delete();
     });
 
-    let secondrowdiv = document.createElement('div')
-    secondrowdiv.className = 'row'
-
-    let coldiv = document.createElement('div')
-    coldiv.className = 'col'
-
-
-    let secondcol2div = document.createElement('div')
-    secondcol2div.className = 'col-2'
-
-
-    firstrowdiv.appendChild(firstcol2div)
-    firstrowdiv.appendChild(col8div)
-    col8div.appendChild(inputgroupdiv)
-    inputgroupdiv.appendChild(collapsebutton)
-    inputgroupdiv.appendChild(input)
-    inputgroupdiv.appendChild(changebutton)
-    inputgroupdiv.appendChild(deletebutton)
-    col8div.appendChild(secondrowdiv)
-    secondrowdiv.appendChild(coldiv)
-    coldiv.appendChild(textarea)
-    coldiv.appendChild(htmlView)
-    firstrowdiv.appendChild(secondcol2div)
-
-    $element.appendChild(firstrowdiv)
+    $element.appendChild($note);
 
 }
 
